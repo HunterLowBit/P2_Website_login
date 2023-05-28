6a2e74a203d3c87d386058af1006ec224b4d678d
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-28 17:46:12 -0300

    update

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "dist", "index.html");
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
+});
+
+app.get("/css/styles.css", (req, res) => {
+  const filePath = path.join(__dirname, "public", "css", "styles.css");
+  res.type("text/css"); // Define o tipo MIME como "text/css"
   res.sendFile(filePath);
 });
 
 app.listen(8000, () => {
   console.log("Servidor ouvindo na porta 8000");
 });
-
