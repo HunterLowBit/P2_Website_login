2d0211ba53b317013f44fbb084857bc7c3d46bce
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-30 00:38:10 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.json());
+
+const secretKey = "seu_segredo_aqui";
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "123456") {
+    const token = jwt.sign({ username }, secretKey);
+    res.json({ token });
+  } else {
+    res.status(401).json({ error: "Credenciais inválidas" });
+  }
+});
+
+app.get("/protected", (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      res.json({ message: "Acesso autorizado!", user: decoded });
+    } catch (error) {
+      res.status(401).json({ error: "Token inválido" });
+    }
+  } else {
+    res.status(401).json({ error: "Token não fornecido" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
