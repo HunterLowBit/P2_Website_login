8fcc7c49833eb46fa6678b6ebbf10ebdf17835c7
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-30 00:50:39 -0300

    SQLITE+JWT teste

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.use(express.static("public"));
@@ -8,15 +9,40 @@ app.use(bodyParser.json());
 
 const secretKey = "seu_segredo_aqui";
 
+// Criação e conexão com o banco de dados SQLite
+const db = new sqlite3.Database(":memory:"); // ou substitua por um caminho para um arquivo
+
+// Cria tabela de usuários
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
+  );
+  db.run(
+    "INSERT OR IGNORE INTO users (username, password) VALUES ('admin', '123456')"
+  );
+});
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  if (username === "admin" && password === "123456") {
-    const token = jwt.sign({ username }, secretKey);
-    res.json({ token });
-  } else {
-    res.status(401).json({ error: "Credenciais inválidas" });
-  }
+  db.get(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, row) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Erro no servidor" });
+        return;
+      }
+
+      if (row) {
+        const token = jwt.sign({ username }, secretKey);
+        res.json({ token });
+      } else {
+        res.status(401).json({ error: "Credenciais inválidas" });
+      }
+    }
+  );
 });
 
 app.get("/protected", (req, res) => {
